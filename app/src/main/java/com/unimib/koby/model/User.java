26f5f2eeb148db68ad07129b9bea0c2f84f54e54
Sandu Stoicov <sandu.stoicov.dev@gmail.com>
2025-05-23 11:08:15 +0200

    Create Model User

---
+++ app/src/main/java/com/unimib/koby/model/User.java
@@ -0,0 +1,49 @@
+package com.unimib.koby.model;
+
+import com.google.firebase.database.Exclude;
+
+public class User {
+    private String name;
+    private String email;
+    private String idToken;
+
+    public User(String name, String email, String idToken) {
+        this.name = name;
+        this.email = email;
+        this.idToken = idToken;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Exclude
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", idToken='" + idToken + '\'' +
+                '}';
+    }
+}
