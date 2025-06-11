771891214e06a7fd3135937cb0e759fde5f1067d
Sandu Stoicov <sandu.stoicov.dev@gmail.com>
2025-06-11 18:42:15 +0200

    Create OpenAIService.java
    Servizio OpenAi in POST

---
+++ app/src/main/java/com/unimib/koby/data/openai/OpenAIService.java
@@ -0,0 +1,16 @@
+package com.unimib.koby.data.openai;
+
+import com.unimib.koby.BuildConfig;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface OpenAIService {
+    String HEADER = "Authorization: Bearer "+ BuildConfig.OPENAI_API_KEY;
+
+    //@Headers({HEADER, "Content-Type: application/json"})
+    @POST("https://api.openai.com/v1/chat/completions")
+    Call<OpenAIResponse> chatCompletion(@Body OpenAIRequest request);
+}
