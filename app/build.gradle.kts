511f0cc9aec2892967ee100bb215541848e27966
Sandu Stoicov <sandu.stoicov.dev@gmail.com>
2025-06-11 18:40:54 +0200

    Update Build Gradle
    Sistemato libreria pdf
    aggiunto il recupero della chiave openAiKey dal gradle properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+val openAiKey: String by project
+
 android {
     namespace = "com.unimib.koby"
     compileSdk = 35
@@ -15,6 +17,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "OPENAI_API_KEY", "\"$openAiKey\"")
+        println("▶ BuildConfig debug – OPENAI = ${openAiKey.takeLast(4)}")
 
     }
 
@@ -34,13 +39,13 @@ android {
         isCoreLibraryDesugaringEnabled = true
     }
     buildFeatures {
+        buildConfig = true
         viewBinding = true
+        dataBinding = true
     }
 }
 
 dependencies {
-
-
 
     //Design UI
     implementation(libs.appcompat)
@@ -48,6 +53,8 @@ dependencies {
     implementation (libs.recyclerview)
     implementation(libs.material)
     implementation(libs.constraintlayout)
+    implementation(libs.activity.ktx)
+    implementation(libs.fragment.ktx)
 
     //Navigation Lifecycle
     implementation(libs.lifecycle.livedata.ktx)
@@ -57,7 +64,16 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
 
+
+    implementation(libs.firebase.firestore.v2500)
+    implementation(libs.firebase.auth.v2300)
+
     //Firebase
+
+    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+    implementation("com.google.firebase:firebase-firestore")
+
+    implementation("com.firebaseui:firebase-ui-firestore:9.0.0")
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.database)
     implementation(libs.google.firebase.auth)
@@ -70,6 +86,7 @@ dependencies {
     // Retrofit & Converter GSON
     implementation (libs.retrofit)
     implementation (libs.converter.gson)
+    implementation(libs.tom.roush.pdfbox.android)
 
     // Validator
     implementation(libs.commons.validator)
@@ -85,7 +102,7 @@ dependencies {
 
     // Picasso Image & PDF Parser
     implementation (libs.picasso)
-    implementation(libs.pdfbox.android)
+    implementation("com.tom-roush:pdfbox-android:2.0.27.0")
 
     //Testing JUnit Espresso
     testImplementation(libs.junit)
