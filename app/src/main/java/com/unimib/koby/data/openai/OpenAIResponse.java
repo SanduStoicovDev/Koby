f4906abfdb245fc7165cb337d2a39b41ff8b6f54
Sandu Stoicov <sandu.stoicov.dev@gmail.com>
2025-06-11 18:41:46 +0200

    Create OpenAIResponse.java
    Response OpenAI

---
+++ app/src/main/java/com/unimib/koby/data/openai/OpenAIResponse.java
@@ -0,0 +1,8 @@
+package com.unimib.koby.data.openai;
+import java.util.List;
+
+public class OpenAIResponse {
+    public List<Choice> choices;
+    public static class Choice { public Message message; }
+    public static class Message { public String role; public String content; }
+}
