8e74f615cc8e1df141f3b9350dc0fe4b8fe144d0
Sandu Stoicov <sandu.stoicov.dev@gmail.com>
2025-06-16 03:21:36 +0200

    Create SettingsViewModelFactory.java

---
+++ app/src/main/java/com/unimib/koby/ui/settings/SettingsViewModelFactory.java
@@ -0,0 +1,15 @@
+package com.unimib.koby.ui.settings;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class SettingsViewModelFactory implements ViewModelProvider.Factory {
+    private final Context ctx;
+    public SettingsViewModelFactory(Context ctx) { this.ctx = ctx; }
+    @NonNull @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new SettingsViewModel(SettingsManager.getInstance(ctx));
+    }
+}
