22d48b7cbc709726201721ffe14cf86090c51170
Sandu Stoicov <sandu.stoicov.dev@gmail.com>
2025-06-18 23:08:38 +0200

    Update ServiceLocator.java
    Delete SpaceStudy Logic

--- app/src/main/java/com/unimib/koby/util/ServiceLocator.java
+++ app/src/main/java/com/unimib/koby/util/ServiceLocator.java
@@ -2,8 +2,6 @@ package com.unimib.koby.util;
 
 import android.app.Application;
 
-import com.unimib.koby.data.repository.space.ISpaceStudyRepository;
-import com.unimib.koby.data.repository.space.SpaceStudyRepository;
 import com.unimib.koby.data.repository.user.IUserRepository;
 import com.unimib.koby.data.repository.user.UserRepository;
 import com.unimib.koby.data.source.user.BaseUserAuthenticationRemoteDataSource;
@@ -20,15 +18,6 @@ public class ServiceLocator {
     private BaseUserDataRemoteDataSource         userRemoteDS;
 
     private ServiceLocator() { }
-
-    private ISpaceStudyRepository spaceRepo;
-
-    public synchronized ISpaceStudyRepository getSpaceStudyRepository() {
-        if (spaceRepo == null) {
-            spaceRepo = new SpaceStudyRepository();
-        }
-        return spaceRepo;
-    }
 
     public static synchronized ServiceLocator getInstance() {
         if (instance == null) instance = new ServiceLocator();
