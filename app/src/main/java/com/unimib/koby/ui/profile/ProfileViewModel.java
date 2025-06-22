edcdcd161ee88d095cc48ee3ed75b43c2e97cd34
Sandu Stoicov <sandu.stoicov.dev@gmail.com>
2025-06-23 00:58:31 +0200

    Update ProfileViewModel.java

--- app/src/main/java/com/unimib/koby/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/unimib/koby/ui/profile/ProfileViewModel.java
@@ -1,19 +1,30 @@
 package com.unimib.koby.ui.profile;
 
+import android.net.Uri;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import com.unimib.koby.data.repository.user.UserRepository;
+import com.unimib.koby.model.Result;
 
 public class ProfileViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final UserRepository repo;
+    private final MutableLiveData<Uri> photoUrl = new MutableLiveData<>();
 
-    public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is profile fragment");
+    public ProfileViewModel(UserRepository repo) {
+        this.repo = repo;
+        // carica all'avvio la foto corrente (se presente)
+        repo.loadProfilePicture().observeForever(photoUrl::setValue);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<Uri> getPhotoUrl() { return photoUrl; }
+
+    public void uploadPhoto(Uri localUri) {
+        repo.uploadProfilePicture(localUri).observeForever(r -> {
+            if (r instanceof Result.Success) {
+                photoUrl.setValue((Uri) ((Result.Success<?>) r).getData());
+            }
+        });
     }
 }
