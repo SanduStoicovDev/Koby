99e03ad2e4151b3f7ff63ad11774cf61c0cf278c
Sandu Stoicov <sandu.stoicov.dev@gmail.com>
2025-06-23 06:59:03 +0200

    Create SettingsManagerTest.java

---
+++ app/src/test/java/com/unimib/koby/settingsUnitTest/SettingsManagerTest.java
@@ -0,0 +1,55 @@
+package com.unimib.koby.settingsUnitTest;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.unimib.koby.ui.settings.SettingsManager;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 34, manifest=Config.NONE)
+public class SettingsManagerTest {
+
+    private final Context ctx = ApplicationProvider.getApplicationContext();
+    private final SettingsManager mgr = SettingsManager.getInstance(ctx);
+
+    @After
+    public void tearDown() {
+        // reset so tests remain isolated
+        mgr.setDarkTheme(false);
+        mgr.setEnglish(false);
+    }
+
+    @Test
+    public void singleton_returnsSameInstance() {
+        SettingsManager mgr2 = SettingsManager.getInstance(ctx);
+        assertThat(mgr).isSameInstanceAs(mgr2);
+    }
+
+    @Test
+    public void darkTheme_getSet_persists() {
+        mgr.setDarkTheme(false);
+        assertThat(mgr.isDarkTheme()).isFalse();
+
+        mgr.setDarkTheme(true);
+        assertThat(mgr.isDarkTheme()).isTrue();
+    }
+
+    @Test
+    public void english_getSet_persists() {
+        mgr.setEnglish(false);
+        assertThat(mgr.isEnglish()).isFalse();
+
+        mgr.setEnglish(true);
+        assertThat(mgr.isEnglish()).isTrue();
+    }
+}
